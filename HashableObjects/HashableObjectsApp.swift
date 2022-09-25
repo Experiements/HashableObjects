c0ee8f8cca2f24e6adb066ddb06a134649948668
Chris Hand <cj.hand@gmail.com>
2022-09-25 09:28:37 -0700

    Initial Commit

---
+++ HashableObjects/HashableObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HashableObjectsApp.swift
+//  HashableObjects
+//
+//  Created by Chris Hand on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct HashableObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
